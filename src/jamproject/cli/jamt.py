c376bbea337c0095a4f8114033c8033f7a3aa381
Kazuya Takei <attakei@gmail.com>
2019-08-14 17:02:28 +0900

    Refactor: change variable name

--- src/jamproject/cli/jamt.py
+++ src/jamproject/cli/jamt.py
@@ -10,15 +10,15 @@ from .. import __version__
 @click.command()
 @click.version_option(message='JAMProject, version %(version)s (text mode)')
 @click.option('--verbose', '-v', is_flag=True)
-@click.argument('src')
-def cli(src, verbose):
+@click.argument('text')
+def cli(text, verbose):
     """CLI.
 
     - If version is flagged, print version string.
     """
     if verbose:
         click.echo(f'JAMProject {__version__}')
-        click.echo(f'Target text: {src}')
+        click.echo(f'Target text: {text}')
 
 
 def main():
