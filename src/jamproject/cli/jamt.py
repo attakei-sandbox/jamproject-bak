aeb2423f857dfea0788eb81462e1863ce3f0f33e
Kazuya Takei <attakei@gmail.com>
2019-08-14 09:44:40 +0900

    Implement version flag

--- src/jamproject/cli/jamt.py
+++ src/jamproject/cli/jamt.py
@@ -2,8 +2,23 @@
 
 This command recieve raw text data and return result.
 """
+import click
+
+from .. import __version__
+
+
+@click.command()
+@click.option('--version', '-V', is_flag=True)
+def cli(version: bool):
+    """CLI.
+
+    - If version is flagged, print version string.
+    """
+    if version:
+        click.echo(f'JAM Project v{__version__}')
+        return
 
 
 def main():
     """Entrypoint."""
-    print("JAM Project")
+    cli()
