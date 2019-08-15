f3fa65091f00b265a2010983a9ee9b7b637951aa
Kazuya Takei <attakei@gmail.com>
2019-08-15 12:23:09 +0900

    Fix for flake8 and set typing

--- src/jamproject/core.py
+++ src/jamproject/core.py
@@ -2,18 +2,21 @@
 
 This module has minimum classes and functions to execute project.
 """
-from janome.tokenizer import Tokenizer
+from typing import List
 
+from janome.tokenizer import Token, Tokenizer
 
 tokenizer = Tokenizer()
 
 
 class TextUnit(object):
     """Unit component to manage text and analyze."""
+
     def __init__(self, raw_text: str):
+        """In initializing, run tokenize."""
         self._raw_text: str = raw_text
         tokenized = tokenizer.tokenize(self.raw_text)
-        self._tokens: list = [t for t in tokenized]
+        self._tokens: List[Token] = [t for t in tokenized]
 
     @property
     def raw_text(self) -> str:
@@ -21,6 +24,6 @@ class TextUnit(object):
         return self._raw_text
 
     @property
-    def tokens(self) -> list:
+    def tokens(self) -> List[Token]:
         """Token parts of raw text."""
         return self._tokens
