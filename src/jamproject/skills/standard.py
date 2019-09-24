a862cc927644eeb668bcd8fde72409295b73822c
Kazuya Takei <attakei@gmail.com>
2019-09-24 13:29:00 +0900

    Define skill to inspect length of sentence

--- src/jamproject/skills/standard.py
+++ src/jamproject/skills/standard.py
@@ -6,6 +6,16 @@ from ..core import TextUnit
 from . import SkillResult, WARNING
 
 
+def limit_length(unit: TextUnit) -> SkillResult:
+    """文の長さが指定値以下であることを検査する。"""
+    expected = 80
+    actually = len(unit.raw_text)
+    if actually > expected:
+        msg = f'一文が長いです。 期待数={expected} 検出数={actually}'
+        return SkillResult.warning(msg)
+    return SkillResult.passed()
+
+
 def limit_toten(unit: TextUnit) -> SkillResult:
     """読点の数が指定値以下であることを検査する。"""
     limit_toten = 2
--- tests/test_skills/test_standard.py
+++ tests/test_skills/test_standard.py
@@ -1,6 +1,6 @@
 """Test case for jamproject.skills.standard module."""
 from jamproject.core import TextUnit
-from jamproject.skills import standard
+from jamproject.skills import standard, WARNING, INFO
 
 
 def test_count_tokens_succeed():
@@ -14,3 +14,15 @@ def test_count_tokens_failure():
     result = standard.limit_toten(unit)
     assert '期待数=' in result.message
     assert '検出数=' in result.message
+
+
+def test_limit_length_succeed():
+    unit = TextUnit('、'.join(['明日は晴れ', ] * 13))
+    result = standard.limit_length(unit)
+    assert result.level == INFO
+
+
+def test_limit_length_succeed():
+    unit = TextUnit('、'.join(['明日は晴れ', ] * 14))
+    result = standard.limit_length(unit)
+    assert result.level == WARNING
